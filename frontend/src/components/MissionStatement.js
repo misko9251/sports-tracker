e8515347e9e5f08c9aede11a21909d0b53426eb5
Matthew Miskovsky <matt.miskovsky@gmail.com>
2023-01-24 21:14:33 -0500

    finished mission statement components on home page

--- frontend/src/components/MissionStatement.js
+++ frontend/src/components/MissionStatement.js
@@ -6,9 +6,9 @@ function MissionStatement(props) {
     const arrowColor = '#0077b6'
   return (
     <div className='mission-statement'>
-        <span><AiFillPlayCircle color={props.logoColor} size={45}/></span>
-        <h5>Upload videos</h5>
-        <p>Bring the game to anyone, anywhere! Upload highlight reel worthy plays!</p>
+        <span>{props.logo}</span>
+        <h5>{props.title}</h5>
+        <p>{props.content}</p>
         <span><BsArrowRight className='arrow' color={arrowColor} size={30}/></span>
     </div>
   )
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import headerVideo from '../assets/backgroundHeader.mp4'
 import MissionStatement from '../components/MissionStatement'
+import {AiFillPlayCircle} from 'react-icons/ai'
+import {BsFillPencilFill} from 'react-icons/bs'
+import {MdOutlineSportsHandball} from 'react-icons/md'
 
 function Home() {
   return (
@@ -18,9 +21,21 @@ function Home() {
         </header>
         <section>
             <div className='mission-statement-container'>
-                <MissionStatement logoColor={'#03045e'}/>
-                <MissionStatement />
-                <MissionStatement />
+                <MissionStatement 
+                logo={<AiFillPlayCircle color={'#03045e'} size={45}/>}
+                title={'Upload Videos'}
+                content={'Bring the game to anyone, anywhere! Upload highlight reel worthy plays!'}
+                />
+                <MissionStatement 
+                logo={<BsFillPencilFill color={'#03045e'} size={45}/>}
+                title={'Track Gameplay'}
+                content={'Cant make the game? Read all of the play-by-play provided by loved ones.'}
+                />
+                <MissionStatement 
+                logo={<MdOutlineSportsHandball color={'#03045e'} size={45}/>}
+                title={'Live Rosters'}
+                content={'Update rosters and player profiles so every kid feels like a professional!'}
+                />
             </div>
         </section>
     </>
