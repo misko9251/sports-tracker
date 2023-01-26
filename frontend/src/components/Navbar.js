4019c381ac09a7ce5987a0ae0e6fbaeca606d1a9
Matthew Miskovsky <matt.miskovsky@gmail.com>
2023-01-25 22:01:49 -0500

    added link component to sign in and logo, both in navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,15 +1,18 @@
 import React, {useState} from 'react'
 import NavLogo from '../assets/logo.png'
 import SportsDropDown from './SportsDropDown'
+import {Link} from 'react-router-dom'
 
 function Navbar() {
   const [isHovered, setIsHovered] = useState(false)
 
   return (
     <nav className='navBar'>
-        <span className='logo-container'>
-          <img src={NavLogo} alt='sports logo'/>
-        </span>
+        <Link to='/'>
+          <span className='logo-container'>
+            <img src={NavLogo} alt='sports logo'/>
+          </span>
+        </Link>
         <ul>
           <li 
           onMouseEnter={()=> setIsHovered(true)}
@@ -19,7 +22,9 @@ function Navbar() {
           </li>
           {isHovered && <SportsDropDown />}
           <li>Features</li>
-          <li>Sign In</li>
+          <Link style={{textDecoration: 'none', color: 'white'}} to='/login'>
+            <li>Sign In</li>
+          </Link>
         </ul>
     </nav>
   )
