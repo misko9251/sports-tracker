55cf85b15a278771e70414ef61c8fc8abdbf9b76
Matthew Miskovsky <matt.miskovsky@gmail.com>
2023-01-30 21:14:31 -0500

    created and styled login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
-import {Routes, Route} from 'react-router-dom'
-import Home from './pages/Home'
+import {Routes, Route, useLocation} from 'react-router-dom'
 import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import Login from './pages/Login'
+
 
 function App() {
+  const location = useLocation()
   return (
     <div className="App">
-      <Navbar />
+      {location.pathname !== '/login' && <Navbar />}
       <Routes>
         <Route path='/' element={<Home/>}/> 
+        <Route path='/login' element={<Login/>}/>
       </Routes>
     </div>
   );
---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,43 @@
+import React, {useState} from 'react'
+import LoginPhoto from '../assets/login.png'
+import {FcSportsMode} from 'react-icons/fc'
+import {FcGoogle} from 'react-icons/fc'
+import {Link} from 'react-router-dom'
+
+function Login() {
+  
+  const [formData, setFormData] = useState({username: '', password: ''})
+
+  return (
+    <div className='login-container'>
+        <section className='login-section'>
+            <div className='login-form-section'>
+                <Link to='/'><FcSportsMode size={50} style={{marginBottom: '5%'}}/> </Link>
+                <h5>Login</h5>
+                <p>Welcome back, hope the game was great!</p>
+                <button><FcGoogle size={20} style={{marginRight: '3%'}}/>Sign in with Google</button>
+                <span>or Sign in with Email</span>
+                <form>
+                    <label>Email*</label>
+                    <input
+                    placeholder='example@example.com'
+                    type='text'
+                    />
+                    <label>Password*</label>
+                    <input
+                    placeholder='Min. 6 characters'
+                    type='password'
+                    />
+                    <button>Login</button>
+                </form>
+                <p>Not registered yet? <Link to='/register' style={{textDecoration: 'none', color: '#429488', fontWeight: 'bold'}}>Create an Account</Link></p>
+            </div>
+        </section>
+        <section className='login-section'>
+            <img src={LoginPhoto}/>
+        </section>
+    </div>
+  )
+}
+
+export default Login
