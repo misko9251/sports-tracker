94b1087653e30f64535b3e50a9ca0d91be5808ee
Matthew Miskovsky <matt.miskovsky@gmail.com>
2023-02-17 21:23:23 -0500

    finished styling form

--- frontend/src/components/AddTeam.js
+++ frontend/src/components/AddTeam.js
@@ -7,28 +7,37 @@ function AddTeam(props) {
         <span onClick={props.onClose}>X</span>
         <section>
             <form className='add-team-form'>
-            <label>Team Name</label>
+            <h1>Add New Team</h1>
+                <label>Team Name</label>
                 <input 
                 type='text'
+                placeholder="What's your team name?"
                 />
-                <label>Select your sport</label>
-                <select>
-                    <option>Hockey</option>
-                    <option>Baseball</option>
-                    <option>Softball</option>
-                    <option>Football</option>
-                    <option>Soccer</option>
-                    <option>Basketball</option>
-                    <option>Lacrosse</option>
-                    <option>Volleyball</option>
-                    <option>Bowling</option>
-                </select>
-                <label>Team type</label>
-                <select>
-                    <option>Travel</option>
-                    <option>Rec/Local</option>
-                    <option>School</option>
-                </select>
+                <hr></hr>
+                <div className='sport-form-details'>
+                    <div className='sport-select-container1'>
+                        <label>Sport</label>
+                        <select>
+                            <option>Hockey</option>
+                            <option>Baseball</option>
+                            <option>Softball</option>
+                            <option>Football</option>
+                            <option>Soccer</option>
+                            <option>Basketball</option>
+                            <option>Lacrosse</option>
+                            <option>Volleyball</option>
+                            <option>Bowling</option>
+                        </select>
+                    </div>
+                    <div className='sport-select-container2'>
+                        <label>Team type</label>
+                        <select>
+                            <option>Travel</option>
+                            <option>Rec/Local</option>
+                            <option>School</option>
+                        </select>
+                    </div>
+                </div>
                 <label>Age</label>
                 <select>
                     <option>Under 13</option>
