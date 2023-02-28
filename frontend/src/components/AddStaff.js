3c3577e67eeb0d5e68ab0c806d03ec9d079af918
Matthew Miskovsky <matt.miskovsky@gmail.com>
2023-02-27 22:17:29 -0500

    styled form to add staff member

--- frontend/src/components/AddStaff.js
+++ frontend/src/components/AddStaff.js
@@ -4,16 +4,18 @@ function AddStaff(props) {
   return (
     <div className={`add-staff-container ${props.isActive ? 'active show' : ''}` }>
         <span onClick={props.onClose}>X</span>
-        <form className='add-staff-form'>
+        <form className='add-staff-form add-team-form'>
             <label>Add Staff Member</label>
             <input
             placeholder='Enter name'
+            className='add-staff-input'
             />
             <label>Title</label>
             <input
             placeholder='e.g. Coach'
+            className='add-staff-input'
             />
-            <button>Submit</button>
+            <button className='submit-new-staff-btn'>Submit</button>
         </form>
     </div>
   )
