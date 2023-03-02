16234a2c77abc33d614dec020e9749fe0ee5ad32
Matthew Miskovsky <matt.miskovsky@gmail.com>
2023-03-01 20:22:13 -0500

    added state to track form inputs

--- frontend/src/components/AddPlayer.js
+++ frontend/src/components/AddPlayer.js
@@ -8,26 +8,61 @@ function AddPlayer(props) {
     position: '',
   })  
 
+  const [fileInputState, setFileInputState] = useState('');
+  const [previewSource, setPreviewSource] = useState('');
+
+  const onChange = (e) => {
+    setFormData((prevVal)=> {
+        return {
+            ...prevVal,
+            [e.target.name]: e.target.value
+        }
+    })
+    const file = e.target.files[0];
+    previewFile(file);
+  }
+
+  const previewFile = (file) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onloadend = () => {
+        setPreviewSource(reader.result);
+    };
+};
+
+  const onSubmit = async (e) => {
+    e.preventDefault()
+    const response = await fetch('http://localhost:2121/dashboard/addPlayer')
+  } 
+
   return (
     <div className={`add-player-container ${props.isActive ? 'active show' : ''}` }>
-    <form className='add-staff-form add-team-form'>
+    <form onSubmit={onSubmit} className='add-staff-form add-team-form'>
         <span className='close-staff-form' onClick={props.onClose}>{<AiOutlineCloseSquare/>}</span>
         <label>Add Player</label>
         <input
         placeholder='Enter name'
         className='add-staff-input'
         type='text'
+        onChange={onChange}
+        value={formData.name}
+        name='name'
         />
         <label>Position</label>
         <input
         placeholder='e.g. Coach'
         className='add-staff-input'
         type='text'
+        onChange={onChange}
+        value={formData.position}
+        name='position'
         />
         <label>Player Image</label>
         <input
-        placeholder='e.g. Coach'
+        id='fileInput'
         type='file'
+        onChange={onChange}
+        value={fileInputState}
         />
         <button style={{marginTop: '2%'}}className='submit-new-player-btn'>Submit</button>
     </form>
