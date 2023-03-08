ce58a8b650a87e4202053b3284727ab73d092fac
Matthew Miskovsky <matt.miskovsky@gmail.com>
2023-03-07 21:11:48 -0500

    added state to track form, this will be used to conditionally render form for practice vs game

--- frontend/src/components/Schedule.js
+++ frontend/src/components/Schedule.js
@@ -1,11 +1,52 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {AiOutlineCloseSquare} from 'react-icons/ai'
 
 function Schedule() {
+
+  const [hasEventsScheduled, setHasEventsScheduled] = useState(false)
+  const [type, setType] = useState('Game')
+  const [formData, setFormData] = useState({
+    type: 'Game'
+  })
+  console.log(formData)
+
+  const handleChange = (e) => {
+    setFormData((prevVal)=> {
+      return {
+        ...prevVal,
+        [e.target.name]: e.target.value
+      }
+    })
+    e.target.value == 'Game' ? setType('Game') : setType('Practice')
+  }
+
   return (
-    <section className='schedule-container dashboard-tabs'>
-        <h4>No Events Scheduled</h4>
-        <button>Add Event</button>
-    </section>
+    <>
+      <section className='schedule-container dashboard-tabs'>
+          <h4>No Events Scheduled</h4>
+          <button>Add Event</button>
+      </section>
+      <form className='add-staff-form add-team-form'>
+            <span className='close-staff-form'>{<AiOutlineCloseSquare/>}</span>
+            <label>Event Type</label>
+            <select value={formData.type} onChange={handleChange} name='type'>
+              <option value='Game'>Game</option>
+              <option value='Practice'>Practice</option>
+            </select>
+            {type == 'Game' ? (
+              <h1>Game selected</h1>
+            ) : (
+              <h1>Practice!!</h1>
+            )}
+            <label>Title</label>
+            <input
+            placeholder='e.g. Coach'
+            className='add-staff-input'
+            name='title'
+            />
+            <button className='submit-new-staff-btn'>Submit</button>
+        </form>
+    </>
   )
 }
 
