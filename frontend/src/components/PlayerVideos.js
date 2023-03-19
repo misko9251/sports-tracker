57fb75adbf0b60e9efff65f91c55802c132a1f5d
Matthew Miskovsky <matt.miskovsky@gmail.com>
2023-03-19 19:44:05 -0400

    video form toggles appropriately for player videos

--- frontend/src/components/PlayerVideos.js
+++ frontend/src/components/PlayerVideos.js
@@ -1,13 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react'
+import AddVideo from './AddVideo'
 
 function PlayerVideos() {
+
+  const [addVideo, setAddVideo] = useState(false)
+
+  const closeAddVideo = () => {
+    setAddVideo(false)
+  }
+
   return (
+    <>
+    
+    <AddVideo
+    onClose={closeAddVideo}
+    isActive={addVideo}
+    />
+
     <div className='player-videos-container'>
         <section className='no-data-added-container dashboard-tabs'>
-          <h4>No Player Videos</h4>
-          <button>Add Video</button>
+            <h4>No Player Videos</h4>
+            <button onClick={() => setAddVideo(true)}>Add Video</button>
         </section>
     </div>
+    </>
   )
 }
 
