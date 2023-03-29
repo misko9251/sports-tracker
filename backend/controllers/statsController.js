78406fa7962d6f88868b086284f751980b328b70
Matthew Miskovsky <matt.miskovsky@gmail.com>
2023-03-28 20:58:08 -0400

    stats now only update when game is finished

--- backend/controllers/statsController.js
+++ backend/controllers/statsController.js
@@ -1,68 +1,39 @@
 const Team = require('../models/Team')
 
 module.exports = {
-    addGoal: async (req, res) => {
-        const {teamId, playerId} = req.params
+    updateHockeyStats: async (req, res) => {
+        const {gameStats} = req.body
+        const {teamId} = req.params
+        const currentTeam = await Team.findById({_id: teamId})
         try {
-            const currentTeam = await Team.findById({_id: teamId})
-            const playerIndex = currentTeam.roster.findIndex((player)=> player._id == playerId)
-            if(playerIndex === -1){
-                return res.status(404).json({msg: 'Player not found'})
+         gameStats.forEach((stat)=>{
+            const {event, playerId} = stat
+            if(event.includes('scored a goal')){
+                const playerIndex = currentTeam.roster.findIndex((player)=> player._id == playerId)
+                if(playerIndex !== -1){
+                    currentTeam.roster[playerIndex].stats.goals++
+                }
+            }else if(event.includes('assisted on a goal')){
+                const playerIndex = currentTeam.roster.findIndex((player)=> player._id == playerId)
+                if(playerIndex !== -1){
+                    currentTeam.roster[playerIndex].stats.assists++
+                }
+            }else if(event.includes('missed a shot')){
+                const playerIndex = currentTeam.roster.findIndex((player)=> player._id == playerId)
+                if(playerIndex !== -1){
+                    currentTeam.roster[playerIndex].stats.missedShots++
+                }
+            }else if(event.includes('made a save')){
+                const playerIndex = currentTeam.roster.findIndex((player)=> player._id == playerId)
+                if(playerIndex !== -1){
+                    currentTeam.roster[playerIndex].stats.saves++
+                }
             }
-            currentTeam.roster[playerIndex].stats.goals++
-            await currentTeam.save()
-            res.status(200).json({msg: 'Goal added'})
+         })
+         await currentTeam.save();
+         res.status(200).json({msg: 'Stats updated'});
         } catch (error) {
-            console.log(error)
-            res.status(500).json({ msg: 'Server error' });
-        }
-    },
-    addAssist: async (req, res) => {
-        const {teamId, playerId} = req.params
-        try {
-            const currentTeam = await Team.findById({_id: teamId})
-            const playerIndex = currentTeam.roster.findIndex((player)=> player._id == playerId)
-            if(playerIndex === -1){
-                return res.status(404).json({msg: 'Player not found'})
-            }
-            currentTeam.roster[playerIndex].stats.assists++
-            await currentTeam.save()
-            res.status(200).json({msg: 'Assist added'})
-        } catch (error) {
-            console.log(error)
-            res.status(500).json({ msg: 'Server error' });
-        }
-    },
-    addMissedShot: async (req, res) => {
-        const {teamId, playerId} = req.params
-        try {
-            const currentTeam = await Team.findById({_id: teamId})
-            const playerIndex = currentTeam.roster.findIndex((player) => player._id == playerId)
-            if(playerIndex === -1){
-                return res.status(404).json({msg: 'Player not found'})
-            }
-            currentTeam.roster[playerIndex].stats.missedShots++
-            await currentTeam.save()
-            res.status(200).json({msg: 'Missed shot added to db'})
-        } catch (error) {
-            console.log(error)
-            res.status(500).json({msg: 'Server error'})
-        }
-    },
-    addSave: async (req, res) => {
-        const {teamId, playerId} = req.params
-        try {
-            const currentTeam = await Team.findById({_id: teamId})
-            const playerIndex = currentTeam.roster.findIndex((player) => player._id == playerId)
-            if(playerIndex === -1){
-                return res.status(404).json({msg: 'Player not found'})
-            }
-            currentTeam.roster[playerIndex].stats.saves++
-            await currentTeam.save()
-            res.status(200).json({msg: 'Save recorded'})
-        } catch (error) {
-            console.log(error)
-            res.status(500).json({msg: 'Server error'})
+         console.log(error)
         }
     }
 }
