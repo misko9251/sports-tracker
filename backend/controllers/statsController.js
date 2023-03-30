eb63c5d2471c562b297ac56b5ac4fc5935a8a1c0
Matthew Miskovsky <matt.miskovsky@gmail.com>
2023-03-29 20:20:52 -0400

    added comments to updateHockeyStats method

--- backend/controllers/statsController.js
+++ backend/controllers/statsController.js
@@ -2,12 +2,19 @@ const Team = require('../models/Team')
 
 module.exports = {
     updateHockeyStats: async (req, res) => {
+        // Game stats to loop through and grab events
         const {gameStats} = req.body
+        // Destructure current team and the eventId which represents the scheduled event we are on
         const {teamId, eventId} = req.params
+        // Grab current team
         const currentTeam = await Team.findById({_id: teamId})
+        // Find the index of the current scheduled event
         const eventIndex = currentTeam.schedule.findIndex((game)=> game._id == eventId)
+        // This method fires when we click end game, so below we are setting is complete to true
         currentTeam.schedule[eventIndex].isComplete = true
+        // Loop through the game action and grab all the events, save in a new array
         const events = gameStats.map((item)=> (item.event))
+        // Set the gameEvents value to the events we looped through above
         currentTeam.schedule[eventIndex].gameEvents = events
         try {
          gameStats.forEach((stat)=>{
