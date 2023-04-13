fc32f0dafbf3cad60a7a74e3bd05ba4657814a99
Matthew Miskovsky <matt.miskovsky@gmail.com>
2023-04-12 20:32:14 -0400

    created contact tab

--- frontend/src/components/Contacts.js
+++ frontend/src/components/Contacts.js
@@ -4,11 +4,25 @@ import TabSpinner from './TabSpinner'
 function Contacts() {
     
     const [isLoading, setIsLoading] = useState(false)
+    const [hasContacts, setHasContacts] = useState(false)
+    const [addContact, setAddContact] = useState(false)
     
     return (
       <>
-      {isLoading ? <TabSpinner /> : 
-      <h1>testing</h1>}
+      {isLoading ? <TabSpinner /> : (
+        hasContacts ? (
+            <>
+            <h1>Contacts</h1>
+            </>
+        ) : (
+            <div className='player-videos-container'>
+            <section className='no-data-added-container dashboard-tabs'>
+                <h4>No Contacts</h4>
+                <button onClick={() => setAddContact(true)}>Add Contact</button>
+            </section>
+          </div>
+        )
+      ) }
       </>
     )
 }
